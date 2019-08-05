3637b4d247aeb0e259c815a42388a866f1ecbba8
SecureCRT <securecrt2008@gmail.com>
2019-08-05 22:49:32 +0800

    fixed the bug

--- leetcode/41. First Missing Positive/First_Missing_Positive.c
+++ leetcode/41. First Missing Positive/First_Missing_Positive.c
@@ -22,21 +22,25 @@ Your algorithm should run in O(n) time and uses constant extra space.
 
 int firstMissingPositive(int *nums, int numsSize)
 {
-    int seed = 1;
-    int i;
+    int i, k, seed = 1;
 
+    //对数据做一次伪排序
     for (i = 0; i < numsSize; i++)
     {
-        /* 
-        实际上不需要 判断正负号 ，可以减少程序执行时间
-        if (nums[i] <= 0)
+        if (nums[i] <= 0 || nums[i] > numsSize)
             continue;
-        else
+        while (nums[i] != i + 1 && nums[i] != nums[nums[i] - 1])
         {
-            if (seed == nums[i])
-                seed++;
+            if (nums[i] <= 0 ||  nums[i] > numsSize)
+                break;
+            k = nums[i];
+            nums[i] = nums[k - 1];
+            nums[k - 1] = k;
         }
-         */
+    }
+    for (i = 0; i < numsSize; i++)
+    {
+        //printf("i = %d, num = %d \r\n", i, nums[i]);
         if (seed == nums[i])
             seed++;
     }
@@ -47,10 +51,14 @@ int main(int argc, char *argv[])
 {
     int nums1[] = {1, 2, 0};
     int numsSize1 = 3;
-    int nums2[] = {3, 4, -1, 1};
-    int numsSize2 = 4;
+    int nums2[] = {3, 4, -1, 1,4,9,6,2};
+    int numsSize2 = 8;
     int nums3[] = {7, 8, 9, 11, 12};
     int numsSize3 = 5;
+
+    int nums4[] = {1, 3, 2, 4, 5};
+    int numsSize4 = 5;
+
     int elem;
 
     elem = firstMissingPositive(nums1, numsSize1);
@@ -59,6 +67,7 @@ int main(int argc, char *argv[])
     printf("firstMissingPositive is %d\r\n", elem);
     elem = firstMissingPositive(nums3, numsSize3);
     printf("firstMissingPositive is %d\r\n", elem);
-
+    elem = firstMissingPositive(nums4, numsSize4);
+    printf("firstMissingPositive is %d\r\n", elem);
     return 0;
 }
